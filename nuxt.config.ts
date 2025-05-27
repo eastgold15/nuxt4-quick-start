79eb4d2c436149fd42e1733c24bf9a0930c537bf
田正东 <1960825664@qq.com>
2025-05-27 11:11:35 +0800

    feat: 更新 nuxt.config.ts，调整兼容性日期，添加开发和生产环境配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,76 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2025-05-27",
   devtools: { enabled: true },
-
-  future: {
-    compatibilityVersion: 4,
+  devServer: {
+    port: Number.parseInt(process.env.NUXT_PORT!) || 5000
   },
-  css: ['@/assets/style/main.css'],
+  experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
+    payloadExtraction: false,
+    renderJsonPayloads: true,
+    typedPages: true
+  },
+  future: {
+    compatibilityVersion: 4
+  },
+  css: ["@/assets/style/main.css"],
 
-  modules: ['@unocss/nuxt', '@nuxt/devtools', '@nuxtjs/color-mode'],
-})
+  modules: [
+    "@unocss/nuxt",
+    "@nuxt/devtools",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt"
+  ],
+
+  // vueuse
+  vueuse: {
+    ssrHandlers: true
+  },
+  runtimeConfig: {
+    count: 1,
+    apiSecret: "", // 可以由 NUXT_API_SECRET 环境变量覆盖
+    public: {
+      // 可以由 NUXT_BASEURL_DEV 环境变量覆盖
+      apiBase: "",
+      apiBase_mock: ""
+    }
+  },
+
+  $production: {
+    app: {
+      head: {
+        title: "生产模板",
+        titleTemplate: "%s - Nuxt 4 + UnoCSS + VueUse"
+      }
+    }
+    // routeRules: {
+    //   "/**": { isr: true },
+    // },
+  },
+  $development: {
+    //
+    app: {
+      head: {
+        title: "开发模板",
+        titleTemplate: "%s - Nuxt 4 + UnoCSS + VueUse"
+      }
+    },
+
+    sourcemap: {
+      server: true,
+      client: true
+    }
+
+  },
+
+  typescript: {
+    strict: true,
+    tsConfig: {
+      compilerOptions: {
+        jsx: "preserve"
+      }
+    }
+  }
+});
